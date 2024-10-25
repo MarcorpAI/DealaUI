39e6c25a82eab558c0b1b48cc08fb136f5ed4946
markbumpy <markbumpy91@gmail.com>
2024-10-25 14:14:59 +0100

    added something..

--- src/api.js
+++ src/api.js
@@ -6,7 +6,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true", // Add the skip warning header globally
   },
 });
 
